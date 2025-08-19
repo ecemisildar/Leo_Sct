309f0334ffe2273d69bc0273ea88f6277801ab11
Ecem <ecemisildar@gmail.com>
2025-08-19 15:55:04 +0200

    colors added in xacro and launch files

--- swarm_segregation/launch/leo_gz.launch.py
+++ swarm_segregation/launch/leo_gz.launch.py
@@ -17,20 +17,19 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-
-
-import os
-
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 from launch.actions import ExecuteProcess
+
+import os
+os.environ['GAZEBO_SIM_SYSTEM'] = 'ignition'
 
 
 def generate_launch_description():
@@ -51,7 +50,7 @@ def generate_launch_description():
         description="Robot namespace",
     )
 
-    # Setup to launch the simulator and Gazebo world
+    # --- Force Ignition Gazebo ---
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
