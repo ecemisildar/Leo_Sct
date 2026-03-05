ce85b01474ec7b7a3a22b43d1e60ca6000629c85
Ecem <ecemisildar@gmail.com>
2026-03-05 16:34:46 +0100

    update robots file

--- Leo_sct/leo_real/scripts/update_all_robots.sh
+++ Leo_sct/leo_real/scripts/update_all_robots.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -u
+set -euo pipefail
 
 ROBOTS=(
   "jetson-01@192.168.178.101"
@@ -9,7 +9,7 @@ ROBOTS=(
 )
 
 WS="~/ros_ws"
-REPO="$WS/src/Leo_Sct"
+REPO="$WS/src/Leo_Sct/Leo_sct"
 BRANCH="real"
 
 run_robot() {
@@ -18,15 +18,17 @@ run_robot() {
   ssh -o BatchMode=yes -o ConnectTimeout=10 "$R" "
     set -e
     cd $REPO
-    git fetch origin
-    git reset --hard
+    echo '[update] repo=' \$(pwd)
+    git fetch --prune origin
     git checkout -f -B $BRANCH origin/$BRANCH
     git reset --hard origin/$BRANCH
-    git clean -fd
+    git clean -fdx
+    find . -type d -name __pycache__ -prune -exec rm -rf {} +
+    echo '[update] commit=' \$(git rev-parse --short HEAD)
 
     cd $WS
     source /opt/ros/humble/setup.bash
-    colcon build --symlink-install
+    colcon build --symlink-install --packages-select leo_real leo_image
 
     echo \"Finished on \$(hostname)\"
   "
